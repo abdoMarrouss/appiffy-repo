f63b9026ca6214bb77897681de05dac1f2dbdbf2
abdelilah-fy-dev <abdelilah.marrouss.fycomputing@gmail.com>
2023-08-02 13:11:26 +0100

    adding routes

--- appiffy-front/src/app/app.component.ts
+++ appiffy-front/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appiffy-front';
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/user', { responseType: 'text' }).subscribe(
+      (data: any) => {
+        // Process JSON data here
+        console.log('hello', data);
+
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  
+}
 }
