683a11b4005db8d33f6e94f3505d73eda264e3be
abdelilah-fy-dev <abdelilah.marrouss.fycomputing@gmail.com>
2023-08-04 02:34:32 +0100

    front : modifying angular builds

--- appiffy-front/src/app/user/components/user.component.ts
+++ appiffy-front/src/app/user/components/user.component.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit{
   constructor(private userServie: UserService , private http: HttpClient) {}
@@ -13,8 +13,33 @@ export class UserComponent implements OnInit{
   ngOnInit() {
     
     this.userServie.findAll().subscribe(data => {
-      console.log("this is the user data ", data);
+      console.log("this is the  data ", data);
     })
   
+  }
+
+
+  animateButton(e: MouseEvent): void {
+    e.preventDefault();
+    const target = e.target as HTMLElement;
+    // Reset animation
+    target.classList.remove('animate');
+    target.classList.add('animate');
+    setTimeout(() => {
+      target.classList.remove('animate');
+    }, 700);
+  }
+
+  logData() {
+    console.log('hello here : ');
+  }
+
+  // Attach the event listener to bubbly buttons after the view initialization
+  ngAfterViewInit() {
+    const bubblyButtons = document.getElementsByClassName("bubbly-button") as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < bubblyButtons.length; i++) {
+      bubblyButtons[i].addEventListener('click', this.animateButton, false);
+    }
   }
 }
