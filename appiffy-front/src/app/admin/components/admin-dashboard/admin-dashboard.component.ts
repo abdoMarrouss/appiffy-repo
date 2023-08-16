d70dcc555ced4270455137918a18dd46fa394ef2
abdelilah-fy-dev <abdelilah.marrouss.fycomputing@gmail.com>
2023-08-16 10:09:24 +0100

    implemented dashboard css

--- appiffy-front/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ appiffy-front/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  styleUrls: ['./admin-dashboard.component.css','./admin-dashboard-style.scss']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit{
+
+
+  ngOnInit() {
+    const menuIcn = document.querySelector(".menuicn");
+    const nav = document.querySelector(".navcontainer");
+
+    if (menuIcn && nav) {
+      menuIcn.addEventListener("click", () => {
+        nav.classList.toggle("navclose");
+      });
+    }
+  }
 
 }
